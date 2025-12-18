ed33c5bbce7107a2656ba912b382d5d60df05d5b
nekrasov <frenckee@yandex.ru>
2025-12-18 21:17:55 +0300

    фикс зависимостей

--- src/main/java/net/betterhorses/fabric/platform/events/FabricPlatformClientEvents.java
+++ src/main/java/net/betterhorses/fabric/platform/events/FabricPlatformClientEvents.java
@@ -1,29 +1,24 @@
 package net.betterhorses.fabric.platform.events;
 
-import net.betterhorses.common.BetterHorses;
 import net.betterhorses.common.ui.HorseStatsHud;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.entity.passive.HorseEntity;
-import net.minecraft.util.Identifier;
 
 public class FabricPlatformClientEvents {
-    private static final Identifier STATS_LAYER = Identifier.of(BetterHorses.MOD_ID, "hud_stats_layer");
 
     public static void init() {
         initHudRenderEvent();
     }
 
     private static void initHudRenderEvent() {
-        HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> 
-            layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, STATS_LAYER, FabricPlatformClientEvents::renderHud)
-        );
+        HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
+            renderHud(drawContext);
+        });
     }
 
-    private static void renderHud(DrawContext context, RenderTickCounter tickCounter) {
+    private static void renderHud(DrawContext context) {
         MinecraftClient client = MinecraftClient.getInstance();
 
         if (client.player == null || !client.player.hasVehicle() || !(client.player.getVehicle() instanceof HorseEntity horse)) {
